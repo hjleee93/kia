b245ab23df24ded59e527267bd2ba15cb6df5a37
loki <jw.park@fromthered.com>
2021-03-23 18:57:50 +0900

    sample Login.vue

---
+++ src/views/Login.vue
@@ -0,0 +1,30 @@
+<template>
+
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+
+@Component
+export default class Login extends Vue {
+    private email: string;
+    private password: string;
+
+    created () {
+
+    }
+
+    mounted () {
+
+    }
+
+    login () {
+
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
