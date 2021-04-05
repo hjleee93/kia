150bec1aa0da608741a66c7fe70d1afe1c6b46a2
hg.park <hg.park@fromthered.com>
2021-04-05 12:49:38 +0900

    카테고리 리스트 추가

---
+++ src/scripts/categoryList.ts
@@ -0,0 +1,47 @@
+export default [
+    {
+        name : 'Inspiration',
+        tag : '#inshashtag',
+        tags : [
+            '#inshashtag1',
+            '#inshashtag2',
+            '#inshashtag3',
+            '#inshashtag4',
+            '#inshashtag5',
+            '#inshashtag6',
+            '#inshashtag7',
+            '#inshashtag8',
+            '#inshashtag9',
+        ]
+    },
+    {
+        name : 'Project',
+        tag : '#project',
+        tags : [
+            '#prohashtag1',
+            '#prohashtag2',
+            '#prohashtag3',
+            '#prohashtag4',
+            '#prohashtag5',
+            '#prohashtag6',
+            '#prohashtag7',
+            '#prohashtag8',
+            '#prohashtag9',
+        ]
+    },
+    {
+        name : 'Exhibition',
+        tag : '#exhibition',
+        tags : [
+            '#exhhashtag1',
+            '#exhhashtag2',
+            '#exhhashtag3',
+            '#exhhashtag4',
+            '#exhhashtag5',
+            '#exhhashtag6',
+            '#exhhashtag7',
+            '#exhhashtag8',
+            '#exhhashtag9',
+        ]
+    }
+]
