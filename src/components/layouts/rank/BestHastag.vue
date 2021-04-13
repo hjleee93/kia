a0b517d857c59b4c56c522abcb57e68ac9244d39
DESKTOP-CTK4VV8\ftred_dev08 <hj.lee@fromthered.com>
2021-04-13 10:58:46 +0900

    best 해시태그

--- src/components/layouts/rank/BestHastag.vue
+++ src/components/layouts/rank/BestHastag.vue
@@ -1,21 +1,18 @@
 <template>
-   <div class="sec-best-hash">
-                        <h2 class="b-tit">BEST 해시태그</h2>
-                        <div class="box-best-hash">
-                            <ol class="best-hash-lists">
-
-                                <li v-for="hashtag in hastagList" :key="hashtag.tagNumber">
-                                    <span class="number">{{hashtag.tagNumber}}</span>
-                                    <p class="hash">
-                                        <span>#</span> {{hashtag.tagName}}
-                                    </p>
-                                    <span class="toot">{{hashtag.tagTootCnt.toLocaleString()}} 툿</span>
-                                </li>
-
-                               
-                            </ol>
-                        </div>
-                    </div>
+    <div class="sec-best-hash">
+        <h2 class="b-tit">BEST 해시태그</h2>
+        <div class="box-best-hash">
+            <ol class="best-hash-lists">
+                <li v-for="hashtag in hastagList" :key="hashtag.tagNumber">
+                    <span class="number">{{ hashtag.tagNumber }}</span>
+                    <p class="hash"><span>#</span> {{ hashtag.tagName }}</p>
+                    <span class="toot"
+                        >{{ hashtag.tagTootCnt.toLocaleString() }} 툿</span
+                    >
+                </li>
+            </ol>
+        </div>
+    </div>
 </template>
 
 
@@ -23,65 +20,42 @@
 import { Component, Prop, Vue } from "vue-property-decorator";
 
 @Component({ components: {} })
-export default class BestHashtag extends Vue{
-    private hastagList: any[] = [
-        {
-            tagNumber: 1,
-            tagName : '해시 태그명 노출',
-            tagTootCnt: 486
-        },
-        {
-            tagNumber: 2,
-            tagName : '해시 태그명 노출2',
-            tagTootCnt: 0
-        },
-        {
-            tagNumber: 3,
-            tagName : '해시 태그명 노출3',
-            tagTootCnt: 0
-        },
-        {
-            tagNumber: 4,
-            tagName : '해시 태그명 노출4',
-            tagTootCnt: 0
-        },
-        {
-            tagNumber: 5,
-            tagName : '해시 태그명 노출5',
-            tagTootCnt: 0
-        },
-        {
-            tagNumber: 6,
-            tagName : '해시 태그명 노출6',
-            tagTootCnt: 0
-        },
-        {
-            tagNumber: 7,
-            tagName : '해시 태그명 노출7',
-            tagTootCnt: 0
-        },
-        {
-            tagNumber: 8,
-            tagName : '해시 태그명 노출8',
-            tagTootCnt: 0
-        },
-        {
-            tagNumber: 9,
-            tagName : '해시 태그명 노출9',
-            tagTootCnt: 0
-        },
-        {
-            tagNumber: 10,
-            tagName : '해시 태그명 노출10',
-            tagTootCnt: 0
-        },
-        
+export default class BestHashtag extends Vue {
+    
+    private hastagList: any[] = [];
+    async mounted() {
+        this.getBestHashtags();
+    }
 
-    ]
-
+    async getBestHashtags() {
+        // const result = await this.$api.getBestHashtags
+        //임시값
+        let result = [
+            {
+                tagNumber: 1,
+                tagName: "해시 태그명 노출",
+                tagTootCnt: 486,
+            },
+            {
+                tagNumber: 2,
+                tagName: "해시 태그명 노출2",
+                tagTootCnt: 0,
+            },
+            {
+                tagNumber: 3,
+                tagName: "해시 태그명 노출3",
+                tagTootCnt: 0,
+            },
+            {
+                tagNumber: 4,
+                tagName: "해시 태그명 노출4",
+                tagTootCnt: 0,
+            },
+        ];
+        this.hastagList = result;
+    }
 }
 </script>
 
 <style>
-
 </style>
