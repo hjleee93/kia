080af10c3ae8cfe41ab44fdb9bdb7890d9dbf2ae
hyeonjeong lee <hlee220@myseneca.ca>
2021-04-21 19:24:26 +0900

    더보기수정

--- src/components/layouts/grid/GridItem.vue
+++ src/components/layouts/grid/GridItem.vue
@@ -1,67 +1,51 @@
 <template>
-    <div class="grid-item">
-        <template v-if="toot.media_attachments !== undefined">
-            <template v-if="toot.media_attachments[0] !== undefined">
-                <a href="#" class="btn btn-link">
-                    <router-link :to="'/mastodon/web/statuses/' + toot.id">
-                        <div class="box-img">
-                            <template
-                                v-if="
-                                    toot.media_attachments[0].type === 'video'
-                                "
-                            >
-                                <i class="icon icon-play"></i>
-                            </template>
-                            <template v-else>
-                                <img
-                                    :class="toot.sensitive ? 'sensitive' : ''"
-                                    :src="toot.media_attachments[0].preview_url"
-                                    alt=""
-                                    style="width: 100%"
-                                />
-                            </template>
-                        </div>
-                    </router-link>
-                    <div class="box-cont">
-                        <template v-if="height > 40">
-                            <p
-                                ref="txtBox1"
-                                class="txt content"
-                                v-html="tootContent"
-                            ></p>
-                            <router-link
-                                :to="'/mastodon/web/statuses/' + toot.id"
-                                ><p class="txt more">
-                                    <span>더보기</span>
-                                </p></router-link
-                            >
-                        </template>
-                        <template v-else>
-                            <p
-                                class="txt content"
-                                ref="txtBox"
-                                v-html="tootContent"
-                            ></p>
-                        </template>
+  <div class="grid-item">
+    <template v-if="toot.media_attachments !== undefined">
+      <template v-if="toot.media_attachments[0] !== undefined">
+        <a href="#" class="btn btn-link">
+          <router-link :to="'/mastodon/web/statuses/' + toot.id">
+            <div class="box-img">
+              <template v-if="toot.media_attachments[0].type === 'video'">
+                <i class="icon icon-play"></i>
+              </template>
+              <template v-else>
+                <img
+                  :class="toot.sensitive ? 'sensitive' : ''"
+                  :src="toot.media_attachments[0].preview_url"
+                  alt=""
+                  style="width: 100%"
+                />
+              </template>
+            </div>
+          </router-link>
+          <div class="box-cont">
+            <template v-if="height > 38">
+              <p ref="txtBox" class="txt content" v-html="tootContent"></p>
 
-                        <router-link
-                            :to="'/mastodon/web/accounts/' + toot.account.id"
-                        >
-                            <span class="nic-name">{{
-                                toot.account.username
-                            }}</span>
-                        </router-link>
-                    </div>
-                </a>
-                <!--비활성-->
-                <Like :toot="toot" />
-                <!--활성화-->
-                <!--<button class="btn btn-like active"></button>-->
-                <!--활성화 모션-->
-                <!--<button class="btn btn-like active motion"></button>-->
+              <router-link :to="'/mastodon/web/statuses/' + toot.id">
+                <p class="txt more">
+                  <span>더보기</span>
+                </p>
+              </router-link>
             </template>
-        </template>
-    </div>
+            <template v-else>
+              <p class="txt content" ref="txtBox" v-html="tootContent"></p>
+            </template>
+
+            <router-link :to="'/mastodon/web/accounts/' + toot.account.id">
+              <span class="nic-name">{{ toot.account.username }}</span>
+            </router-link>
+          </div>
+        </a>
+        <!--비활성-->
+        <Like :toot="toot" />
+        <!--활성화-->
+        <!--<button class="btn btn-like active"></button>-->
+        <!--활성화 모션-->
+        <!--<button class="btn btn-like active motion"></button>-->
+      </template>
+    </template>
+  </div>
 </template>
 
 <script lang="ts">
@@ -71,57 +55,61 @@ import Like from "./Like.vue";
 
 @Component({ components: { Like } })
 export default class GridItem extends Vue {
-    @Prop() toot!: any;
-    
-    private tootContent: string = "";
-    private height: number = 0;
-    private baseURL: string = process.env.VUE_APP_BASE_API!;
+  @Prop() toot!: any;
 
-    mounted() {
-        if (this.toot.content !== undefined) {
-            this.tootContent = this.toot.content.replaceAll(
-                `${this.baseURL}tags`,
-                "#/mastodon/tags"
-            );
-        }
-        this.matchHeight();
+  private tootContent: string = "";
+  private height: number = 0;
+  private baseURL: string = process.env.VUE_APP_BASE_API!;
+
+  mounted() {
+    if (this.toot.content !== undefined) {
+      this.tootContent = this.toot.content.replaceAll(
+        `${this.baseURL}tags`,
+        "#/mastodon/tags"
+      );
+      this.tootContent = this.tootContent.substring(
+        3,
+        this.tootContent.length - 4
+      );
     }
+    this.matchHeight();
+  }
 
-    matchHeight() {
+  matchHeight() {
+    this.$nextTick(() => {
+      //@ts-ignore
+      if (this.$refs.txtBox !== undefined) {
         //@ts-ignore
-        if (this.$refs.txtBox !== undefined) {
-            //@ts-ignore
-            this.height = this.$refs.txtBox.clientHeight;
-            console.log(this.height)
-            
-           
-        }
-    }
+        this.height = this.$refs.txtBox.clientHeight;
+        console.log(this.height);
+      }
+    });
+  }
 }
 </script>
 
 <style scope>
 .grid-item.hidden {
-    display: none !important;
+  display: none !important;
 }
 .txt.content {
-    text-align: left !important;
-    width: 100%;
-    white-space: normal;
-    display: -webkit-box;
-    -webkit-line-clamp: 3;
-    -webkit-box-orient: vertical;
-    overflow: hidden;
+  text-align: left !important;
+  width: 100%;
+  white-space: normal;
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
 }
 .txt.more {
-    float: right;
+  float: right;
 }
 .sensitive {
-    filter: blur(1.5em);
+  filter: blur(1.5em);
 }
 @media (min-width: 320px) and (max-width: 1023px) {
-    #header + #container #content .content {
-        padding-bottom: 0px;
-    }
+  #header + #container #content .content {
+    padding-bottom: 0px;
+  }
 }
 </style>
